628bce3e269e4493a44b5a58b8011067cadba01b
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-12-24 23:47:26 +0600

    Complete: Validate Username & Email in Next.js + MySQL | Prevent Duplicate Registration with Drizzle ORM.

--- job-portal-app/job-portal-nextjs/src/app/register/registrationAction.action.tsx
+++ job-portal-app/job-portal-nextjs/src/app/register/registrationAction.action.tsx
@@ -3,6 +3,7 @@
 import db from "@/config/db";
 import { users } from "@/drizzle/schema";
 import argon2 from "argon2";
+import { eq, or } from "drizzle-orm";
 
 // export const registrationAction = async (formData: FormData) => {
 //   console.log(Object.fromEntries(formData.entries()));
@@ -20,6 +21,17 @@ export const registrationAction = async (data: {
 }) => {
   try {
     const { name, userName, email, password, role } = data;
+
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(or(eq(users?.email, email), eq(users?.userName, userName)));
+
+    if (user) {
+      if (user?.email === email)
+        return { status: "error", message: "email already exists" };
+      else return { status: "error", message: "username already exists" };
+    }
 
     const hashedPassword = await argon2.hash(password);
 
