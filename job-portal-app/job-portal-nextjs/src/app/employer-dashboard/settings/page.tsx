60fa0b7e7fae34c861c33ff4308849eaa8c46106
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-01-02 21:49:48 +0600

    Complete: Create Employer Settings Page in Next.JS | React Hook Form + Shadcn UI.

---
+++ job-portal-app/job-portal-nextjs/src/app/employer-dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import EmployerSettingsForm from "@/features/employers/components/employer-settings-form";
+
+const EmployerSettings = () => {
+  return (
+    <div>
+      <EmployerSettingsForm />
+    </div>
+  );
+};
+
+export default EmployerSettings;
