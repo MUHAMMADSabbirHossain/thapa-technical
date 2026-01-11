9d32afd7a4fa3adea1f80574aa501b330efface9
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-01-11 14:56:21 +0600

    Complete: Create Job Post Schema in Drizzle ORM (MySQL) | Job Portal App with Next.js

--- job-portal-app/job-portal-nextjs/src/config/constant.ts
+++ job-portal-app/job-portal-nextjs/src/config/constant.ts
@@ -1,2 +1,53 @@
 export const SESSION_LIFETIME = 30 * 24 * 60 * 60;
 export const SESSION_REFRESH_TIME = SESSION_LIFETIME / 2;
+
+export const SAlARY_CURRENCY = [
+  "USD",
+  "EUR",
+  "GBP",
+  "CAD",
+  "AUD",
+  "JPY",
+  "INR",
+  "NPR",
+  "PKR",
+  "BDT",
+] as const;
+
+export const SAlARY_PERIOD = [
+  "hourly",
+  "daily",
+  "weekly",
+  "monthly",
+  "yearly",
+] as const;
+
+export const JOB_TYPE = ["remote", "hybrid", "on-site"] as const;
+
+export const WORK_TYPE = [
+  "full-time",
+  "part-time",
+  "contract",
+  "temporary",
+  "freelance",
+] as const;
+
+export const JOB_LEVEL = [
+  "internship",
+  "entry level",
+  "junior",
+  "mid level",
+  "senior level",
+  "lead",
+  "manager",
+  "director",
+  "executive",
+] as const;
+
+export const MINIMUM_EDUCATION = [
+  "none",
+  "high school",
+  "undergraduate",
+  "masters",
+  "phd",
+] as const;
--- job-portal-app/job-portal-nextjs/src/drizzle/schema.ts
+++ job-portal-app/job-portal-nextjs/src/drizzle/schema.ts
@@ -1,5 +1,15 @@
+import {
+  JOB_LEVEL,
+  JOB_TYPE,
+  MINIMUM_EDUCATION,
+  SAlARY_CURRENCY,
+  SAlARY_PERIOD,
+  WORK_TYPE,
+} from "@/config/constant";
 import { relations } from "drizzle-orm";
 import {
+  boolean,
+  date,
   datetime,
   int,
   mysqlEnum,
@@ -79,6 +89,47 @@ export const applicants = mysqlTable("applicants", {
   updatedAt: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
   deletedAt: timestamp("deleted_at").notNull().defaultNow().onUpdateNow(),
 });
+
+export const jobs = mysqlTable("jobs", {
+  id: int("id").autoincrement().primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  employerId: int("employer_id")
+    .notNull()
+    .references(() => employers?.id, { onDelete: "cascade" }),
+  description: text("description").notNull(),
+  tags: text("tags"),
+  minSalary: int("min_salary"),
+  maxSalary: int("max_salary"),
+  salaryCurrency: mysqlEnum("salary_currency", SAlARY_CURRENCY),
+  salaryPeriod: mysqlEnum("salary_period", SAlARY_PERIOD),
+  location: varchar("location", { length: 255 }),
+  jobType: mysqlEnum("job_type", JOB_TYPE),
+  workType: mysqlEnum("work_type", WORK_TYPE),
+  jobLevel: mysqlEnum("job_level", JOB_LEVEL),
+  experience: text("experience"),
+  minEducation: mysqlEnum("min_education", MINIMUM_EDUCATION),
+  isFeatured: boolean("is_featured").default(false).notNull(),
+  expiredAt: date("expired_at"),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
+  deletedAt: timestamp("deleted_at").notNull().defaultNow().onUpdateNow(),
+});
+
+export const jobsRelations = relations(jobs, ({ one }) => ({
+  // Each job belongs to one employer
+  employer: one(employers, {
+    fields: [jobs?.employerId],
+    references: [employers?.id],
+  }),
+}));
+
+// relations(TABLE_NAME, (helpers) => ({
+//   relationName: relationType(OTHER_TABLE, {
+//     fields: [CURRENT_TABLE.column],
+//     references: [OTHER_TABLE.column],
+//   }),
+// }));
 
 // Relations definations
 export const userRelations = relations(users, ({ one, many }) => ({
