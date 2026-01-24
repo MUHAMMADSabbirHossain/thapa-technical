c6df938a950f7407dfba36d44d882119df37c9c7
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-01-24 21:04:18 +0600

    Complete: Applicant Dashboard in Next.js 🔥 Role Based Redirect + Protected Layout.tsx (Job Portal App)

---
+++ job-portal-app/job-portal-nextjs/src/app/(applicants)/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+  const user = await getCurrentUser();
+
+  if (!user) redirect("/login");
+
+  if (user.role !== "applicant") redirect("/employer-dashboard");
+
+  return (
+    <div className="flex min-h-screen bg-background">
+      <main className="container mx-auto mt-5 ml-70 mr-5">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
--- job-portal-app/job-portal-nextjs/src/app/(applicants)/dashboard/page.tsx
+++ job-portal-app/job-portal-nextjs/src/app/(applicants)/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import { logoutUserAction } from "@/features/auth/server/auth.actions";
+import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import { redirect } from "next/navigation";
 
-const applicantDashboard = () => {
+const applicantDashboard = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <h1>applicantDashboard</h1>
